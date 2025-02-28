ad217430437b276f0adda80ac8852f0e6f515665
irissab <iris.sabas@gmail.com>
2025-03-01 01:48:47 +0800

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -67,13 +67,13 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            app.longer.warning("Invalid login attempt - Invalid username or password. Username: {}".format(form.username.data))
+            app.logger.warning("Invalid login attempt - Invalid username or password. Username: {}".format(form.username.data))
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
-            app.longer.warning("{} logged successfully.".format(form.username.data))
+            app.logger.warning("{} logged successfully.".format(form.username.data))
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
@@ -84,14 +84,14 @@ def authorized():
     if request.args.get('state') != session.get("state"):
         return redirect(url_for("home"))  # No-OP. Goes back to Index page
     if "error" in request.args:  # Authentication/Authorization failure
-        app.longer.warning("Invalid login attempt. No code returned")
+        app.logger.warning("Invalid login attempt. No code returned")
         return render_template("auth_error.html", result=request.args)
     if request.args.get('code'):
         cache = _load_cache()
         # TODO: Acquire a token from a built msal app, along with the appropriate redirect URI
         result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(request.args.get('code'), scopes=Config.SCOPE, redirect_uri=url_for('authorized', _external=True, _scheme='https'))
         if "error" in result:
-            app.longer.warning("Invalid login attempt. Code returned")
+            app.logger.warning("Invalid login attempt. Code returned")
             return render_template("auth_error.html", result=result)
         session["user"] = result.get("id_token_claims")
         # Note: In a real app, we'd use the 'name' property from session["user"] below
@@ -99,7 +99,7 @@ def authorized():
         user = User.query.filter_by(username="admin").first()
         login_user(user)
         _save_cache(cache)
-        app.longer.warning("admin logged successfully.")
+        app.logger.warning("admin logged successfully.")
     return redirect(url_for('home'))
 
 @app.route('/logout')
